76df85538e38447c21f1da662d91ab00e361a2f4
root <root@postwork.itinet.fr>
2015-12-21 16:38:00 +0100

    Veuillez saisir le message de validation pour vos modifications. Les lignes

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
-<?php
-	phpinfo();
-?>
+
+#<?php
+
+	
+#	echo "<h1> Bienvenue sur Postwork </h1>";
+#	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+#     <script type="text/javascript" src="/mini/javascripts/mini.js"></script>
+ #    <script type="text/javascript">
+  #     jQuery(document).ready(function() {
+   #      JAPPIX_STATIC = "/mini/";
+    #     HOST_BOSH = "http://bosh.postwork.itinet.fr:5280/http-bind";
+
+     #    JappixMini.launch({
+      #     connection: {
+             #user: 'username',
+             #password: 'password',
+       #      domain: 'anonymous.postwork.itinet.fr',
+        #   },
+         #  application: {
+          #   network: {
+          #     autoconnect: true,
+           #  },
+            # interface: {
+             #  showpane: true,
+            # },
+	    # user: {
+             # random_nickname: true,
+          # },
+
+           #groupchat: {
+            #  open: ["portfolio@muc.postwork.itinet.fr"],
+          # },
+          # },
+        # });
+    # }); 
+     #</script>
+#?>
---
+++ pull.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo git pull
+sudo chown -R www-data:www-data .
